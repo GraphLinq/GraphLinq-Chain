20c278932980003c5d0d267c158a3641c1c118a5
jguyet <gygydu19@hotmail.fr>
2023-02-09 08:06:07 +0100

    Equitable Signer Rewards

--- consensus/clique/clique.go
+++ consensus/clique/clique.go
@@ -575,21 +575,15 @@ func (c *Clique) Prepare(chain consensus.ChainHeaderReader, header *types.Header
 
 // Finalize implements consensus.Engine, ensuring no uncles are set
 func (c *Clique) Finalize(chain consensus.ChainHeaderReader, header *types.Header, state *state.StateDB, txs []*types.Transaction, uncles []*types.Header, withdrawals []*types.Withdrawal) {
-
-	// Recovery of the signers of the previous block
-	number := header.Number.Uint64()
-	snap, _ := c.snapshot(chain, number-1, header.ParentHash, nil)
-
-	// Awarding rewards to signers of the last block
-	blockReward := new(big.Int).Set(FrontierBlockReward)
-	numberOfSingers := len(snap.signers())
-	if numberOfSingers > 0 {
-		for i := 0; i < numberOfSingers; i++ {
-			signer := snap.signers()[i]
-			reward := new(big.Int).Div(blockReward, big.NewInt(int64(numberOfSingers)))
-			state.AddBalance(signer, reward)
-		}
+	// Recovery of the signer of the previous block
+	signer, ok := c.signatures.Get(header.ParentHash)
+	// Awarding rewards to signer of the last block
+	if ok && signer != (common.Address{}) {
+		blockReward := new(big.Int).Set(FrontierBlockReward)
+		reward := new(big.Int).Set(blockReward)
+		state.AddBalance(signer, reward)
 	}
+
 	header.Root = state.IntermediateRoot(chain.Config().IsEIP158(header.Number))
 	header.UncleHash = types.CalcUncleHash(nil)
 }
